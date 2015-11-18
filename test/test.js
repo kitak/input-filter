11eb3815901bec5776404140a593573f32a82568
Keisuke KITA <kei.kita2501@gmail.com>
2015-11-19 08:24:39 +0900

    setup test tools

---
+++ test/test.js
@@ -0,0 +1,9 @@
+var assert = require('power-assert');
+
+describe('TextHighlighter', function() {
+  describe('#highlight', function() {
+    it('', function() {
+      assert.equal(1+1, 2);
+    });
+  });
+})
